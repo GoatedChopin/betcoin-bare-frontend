38e4cbc841099cda34ab0e7c7583684fd513cc49
GoatedChopin <colbysmeline@gmail.com>
2023-04-14 15:04:59 -0500

    first commit

---
+++ src/SurveyLookup.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import Web3 from 'web3';
+
+
+const SurveyLookup: React.FC  = () => {
+
+    // function onSubmit = (survey_id) => {
+        
+    // }
+
+    return (
+        <div className="p-1">
+            <form>
+                <input type="text" />
+                <input type="submit">Find Survey</input>
+            </form>
+        </div>
+    )
+}
+
+export default SurveyLookup;
