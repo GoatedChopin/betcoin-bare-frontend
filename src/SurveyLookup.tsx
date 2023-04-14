c9971ab71b4ff75a7b3e5f1fb26fade6676ecb6f
GoatedChopin <colbysmeline@gmail.com>
2023-04-14 16:13:40 -0500

    Fetch Survey component is (mostly) complete

--- src/SurveyLookup.tsx
+++ src/SurveyLookup.tsx
@@ -1,18 +1,33 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Web3 from 'web3';
 
+const betcoin_json_interface = {};
+const betcoin_address = "";
 
 const SurveyLookup: React.FC  = () => {
 
-    // function onSubmit = (survey_id) => {
-        
-    // }
+    const [pollId, setPollId] = useState<string>("");
+    // const web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
+    // var Contract = require('web3-eth-contract');
+    // var contract = new Contract(betcoin_json_interface, betcoin_address)
+
+    const handleInputChange = (
+        event: React.ChangeEvent<HTMLInputElement>
+      ): void => {
+        setPollId(event.target.value);
+      };
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+        event.preventDefault();
+        console.log(pollId)
+        // contract.methods.getPollInfo().send({id: pollId});
+    }
 
     return (
         <div className="p-1">
-            <form>
-                <input type="text" />
-                <input type="submit">Find Survey</input>
+            <form onSubmit={ handleSubmit }>
+                <input type="text" value={ pollId } onChange={ handleInputChange }/>
+                <input type="submit" value="Fetch Survey"/>
             </form>
         </div>
     )
